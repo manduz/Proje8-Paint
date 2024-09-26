1580442c5c2f6b3f9642608893a2a4d1deda2bda
manduz <emekmanduz3623@gmail.com>
2024-09-26 17:35:36 +0300

    Basic Paint uygulaması

---
+++ PaintApp/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintApp
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp = new Bitmap(1024, 768);
+        Pen p = new Pen(Color.Black, 5);//kalemin uzunluu rengini belirledik.
+        bool drawing = false;
+        public Form1()
+        {
+         
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDownClick(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+                drawing = false;
+            else
+                drawing = true;
+
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(drawing)
+            {
+                //çizmek için
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawEllipse(p, e.X, e.Y, 3, 1);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Red;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Green;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Blue;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Purple;
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Pink;
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Yellow;
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "PNG Dosyaları|*.png|JPEG Dosyaları|*.jpg;*.jpeg";
+            saveFileDialog1.Title = "Dosyayı Kaydedin";
+            saveFileDialog1.ShowDialog();
+
+            if(saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                switch(saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        this.pictureBox1.Image.Save(fs,System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        this.pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                }
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
